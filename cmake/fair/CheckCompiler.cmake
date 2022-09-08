dee75b25bbcf712916b7b0ef6f7e73d2d27432bb
Daniel Wielanek <daniel.wielanek@gmail.com>
2022-09-08 10:54:22 +0200

    Removed unsued files.

--- cmake/fair/CheckCompiler.cmake
+++ cmake/fair/CheckCompiler.cmake
@@ -114,7 +114,7 @@ If(FAIRSOFT_CONFIG)
   String(STRIP ${FAIRSOFT_CXX_FLAGS} FAIRSOFT_CXX_FLAGS)
   Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FAIRSOFT_CXX_FLAGS}")
 Else()
-  Message(STATUS "fairsoft-config not found. Is SIMPATH or FAIRSOFT_ROOT set correctly?")
+  Message(STATUS "fairsoft-config not found.")
 EndIf()
 
 

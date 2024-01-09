0b553591e86505cdd29baeef3d0c83b5d4e9395c
Daniel Wielanek <daniel.wielanek@gmail.com>
2024-01-09 22:45:55 +0100

    New methods added.

--- analysis/femto/corrfit/CorrFitDrawOptions.h
+++ analysis/femto/corrfit/CorrFitDrawOptions.h
@@ -31,8 +31,11 @@ namespace Hal {
     Bool_t fRgb          = {kFALSE};
     Bool_t fYRangeSet    = {kFALSE};
     Bool_t fLegendPosSet = {kFALSE};
-    Bool_t fDrawRealOnly = {kTRUE};
+    Bool_t fDrawReal     = {kTRUE};
+    Bool_t fDrawImag     = {kTRUE};
     Bool_t fDrawNegative = {kFALSE};
+    Bool_t fDrawGrid     = {kFALSE};
+    Bool_t fDrawSepImg   = {kFALSE};
     Double_t fMin = {0}, fMax = {0};
     Double_t fMin2 = {0}, fMax2 = {0};
     Double_t fRangeMin = {0}, fRangeMax = {0};
@@ -65,22 +68,76 @@ namespace Hal {
     Bool_t Diag2() const { return fDiag2; };
     Bool_t Rgb() const { return fRgb; }
     Bool_t Chi2() const { return fDrawChi2; }
+    Bool_t DrawGrid() const { return fDrawGrid; }
     Bool_t IsYRangeSet() const { return fYRangeSet; };
+    Bool_t SeparateImaginary() const { return fDrawSepImg; }
     Bool_t LegendPos() const { return (fXLegend[0] == fXLegend[1] ? kFALSE : kTRUE); }
+    std::pair<Bool_t, Bool_t> DrawRealIm() { return std::pair<Bool_t, Bool_t>(fDrawReal, fDrawImag); }
     Double_t GetMax() const { return fMax; }
     Double_t GetMin() const { return fMin; }
     Double_t GetMax2() const { return fMax2; }
     Double_t GetMin2() const { return fMin2; }
     Double_t GetLegendPos(Int_t id) const;
+    /**
+     * set legend position
+     * @param x1
+     * @param x2
+     * @param y1
+     * @param y2
+     */
     void SetLegendPos(Double_t x1, Double_t x2, Double_t y1, Double_t y2);
+    /**
+     * automatically scale CF to fit 1 if fit is correct
+     * @param autoNorm
+     */
     void SetAutoNorm(Bool_t autoNorm = kTRUE) { fAutoNorm = autoNorm; }
+    /**
+     * draw CF
+     * @param drawCf
+     */
     void SetDrawCf(Bool_t drawCf = kTRUE) { fDrawCF = drawCf; }
+    /**
+     * draw real/imaginary part - NOTE: works onfly with SH
+     * @param drawRe
+     * @param drawIm
+     */
+    void SetDrawReIm(Bool_t drawRe, Bool_t drawIm = kFALSE) {
+      fDrawReal = drawRe;
+      fDrawImag = drawIm;
+    };
+    /**
+     * draw imaginary part seperate NOTE: works only with SH
+     * @param drawSep
+     */
+    void SetImaginarySeparate(Bool_t drawSep = kTRUE) { fDrawSepImg = drawSep; }
+    /**
+     * draw grid
+     * @param draw
+     */
+    void SetDrawGrid(Bool_t draw = kTRUE) { fDrawGrid = draw; }
+    /**
+     * draw legend
+     * @param drawLegend
+     */
     void SetDrawLegend(Bool_t drawLegend = kTRUE) { fDrawLegend = drawLegend; }
+    /**
+     * draw numerical error if possible
+     * @param drawNumErr
+     */
     void SetDrawNumErr(Bool_t drawNumErr = kTRUE) { fDrawNumErr = drawNumErr; }
+    /**
+     * set min and max for drawing
+     * @param min
+     * @param max
+     */
     void SetMinMax(Double_t min = 0, Double_t max = 0) {
       fMin = min;
       fMax = max;
     }
+    /**
+     * draw with same option
+     * @param same
+     */
     void SetSame(Bool_t same = kTRUE) { fSame = same; }
     ClassDef(CorrFitDrawOptions, 1);
   };

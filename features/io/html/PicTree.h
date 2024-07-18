634f4db88f2e0bc7aae3daae4a05aeaf642b6b35
Daniel Wielanek <daniel.wielanek@gmail.com>
2024-07-18 10:35:43 +0200

    Pic tree description added.

--- features/io/html/PicTree.h
+++ features/io/html/PicTree.h
@@ -10,7 +10,9 @@
 
 #include "Object.h"
 
-
+/**
+ * generate picture with flow of data in task
+ */
 namespace Hal {
 
   class PicTree : public Object {
@@ -44,15 +46,40 @@ namespace Hal {
 
   public:
     PicTree();
+    /**
+     * add point drawn as circle
+     * @param depth depth of circle (x)
+     * @param number position of circle (y)
+     * @param style 0 for normal data, 1 for background
+     * @param ratio ratio of passed objects
+     */
     void AddPoint(Int_t depth, Int_t number, Int_t style, Double_t ratio);
+    /**
+     * adds link
+     * @param depths depth of data {from, to}
+     * @param ids position of circles {from, to}
+     * @param style 0 for normal link 1 for background
+     */
     void AddLink(std::initializer_list<Int_t> depths, std::initializer_list<Int_t> ids, Int_t style);
     /**
      * save picture
      * @param path
      */
     void SaveAs(TString path);
+    /**
+     *
+     * @return width of picture
+     */
     Double_t GetWidth() const { return fWindowWidth; }
+    /**
+     *
+     * @return height of picture
+     */
     Double_t GetHeight() const { return fWindowHeight; }
+    /**
+     *
+     * @return array of coordinates (calcualted during calling SaveAs [tier level][collection] first -> x second ->y coordinate
+     */
     std::vector<std::vector<std::pair<double, double>>> GetCoords() const { return fPoints; };
     virtual ~PicTree() {};
     ClassDef(PicTree, 0)

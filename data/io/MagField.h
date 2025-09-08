f8750e721e45405a03018997b958896e54da53d1
Daniel Wielanek <daniel.wielanek@gmail.com>
2025-09-08 11:44:08 +0200

    Adding new magnetic field.

--- data/io/MagField.h
+++ data/io/MagField.h
@@ -34,5 +34,24 @@ namespace Hal {
     virtual ~MagField();
     ClassDef(MagField, 1)
   };
+
+  class ConstMagField : public MagField {
+    Float_t fBx = {0};
+    Float_t fBy = {0};
+    Float_t fBz = {0};
+
+  public:
+    ConstMagField() {};
+    void SetField(Float_t x, Float_t y, Float_t z);
+    ConstMagField(Float_t x, Float_t y, Float_t z) : fBx(x), fBy(y), fBz(z) {};
+    virtual TVector3 GetField(Double_t /*x*/, Double_t /*y*/, Double_t /*z*/) const { return TVector3(fBx, fBy, fBz); }
+    /**
+     * alternative method to get field
+     */
+    void GetFieldValue(const Double_t point[3], Double_t* bFiel) const;
+    virtual ~ConstMagField() {};
+    ClassDef(ConstMagField, 1)
+  };
+
 }  // namespace Hal
 #endif /* HAL_ANALYSIS_BASE_STEER_HALFIELD_H_ */

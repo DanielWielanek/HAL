bd0245ada40c1cd1c45e1709c6077b3c641d2bf4
Daniel Wielanek <daniel.wielanek@gmail.com>
2026-01-26 22:04:36 +0100

    Refactoring of generators.

--- features/std/StdPhys.cxx
+++ features/std/StdPhys.cxx
@@ -20,19 +20,43 @@ namespace Hal {
   namespace Std {
     namespace LorentzGenerator {
       TLorentzVector PtY(Double_t pt, Double_t y, Int_t pid, Double_t phi) {
-        if (phi == -100) { phi = gRandom->Uniform(-TMath::Pi(), TMath::Pi()); }
-        double px     = pt * TMath::Cos(phi);
-        double py     = pt * TMath::Sin(phi);
         auto pdgTrack = TDatabasePDG::Instance()->GetParticle(pid);
         if (!pdgTrack) {
           Hal::Cout::PrintInfo("Hal::Std::GenerateParticleYpt (in StPhysics) cannot find pdg!", Hal::EInfo::kError);
           return TLorentzVector(0, 0, 0, 0);
         }
         double mass = pdgTrack->Mass();
-        double mT   = TMath::Sqrt(mass * mass + pt * pt);
-        double pz   = mT * TMath::SinH(y);
+        return PtYM(pt, y, mass, phi);
+      }
+
+      TLorentzVector PtYM(Double_t pt, Double_t y, Double_t m, Double_t phi) {
+        if (phi == -100) { phi = gRandom->Uniform(-TMath::Pi(), TMath::Pi()); }
+        double px = pt * TMath::Cos(phi);
+        double py = pt * TMath::Sin(phi);
+        double mT = TMath::Sqrt(m * m + pt * pt);
+        double pz = mT * TMath::SinH(y);
         TLorentzVector p;
-        p.SetXYZM(px, py, pz, mass);
+        p.SetXYZM(px, py, pz, m);
+        return p;
+      }
+
+      TLorentzVector PtEta(Double_t pt, Double_t eta, Int_t pid, Double_t phi) {
+        auto pdgTrack = TDatabasePDG::Instance()->GetParticle(pid);
+        if (!pdgTrack) {
+          Hal::Cout::PrintInfo("Hal::Std::GenerateParticleYeta (in StPhysics) cannot find pdg!", Hal::EInfo::kError);
+          return TLorentzVector(0, 0, 0, 0);
+        }
+        double mass = pdgTrack->Mass();
+        return PtEtaM(pt, eta, mass, phi);
+      }
+
+      TLorentzVector PtEtaM(Double_t pt, Double_t eta, Double_t m, Double_t phi) {
+        if (phi == -100) { phi = gRandom->Uniform(-TMath::Pi(), TMath::Pi()); }
+        Double_t px = pt * TMath::Cos(phi);
+        Double_t py = pt * TMath::Sin(phi);
+        Double_t pz = TMath::Sqrt(px * px + py * py) * TMath::SinH(eta);
+        TLorentzVector p;
+        p.SetXYZM(px, py, pz, m);
         return p;
       }
     }  // namespace LorentzGenerator
--- features/std/StdPhys.h
+++ features/std/StdPhys.h
@@ -24,6 +24,33 @@ namespace Hal {
        * @return
        */
       TLorentzVector PtY(Double_t pt, Double_t y, Int_t pid, Double_t phi = -100);
+      /**
+       * generate TLorentzVector with given parameters
+       * @param pt
+       * @param y
+       * @param m mass
+       * @param phi
+       * @return
+       */
+      TLorentzVector PtYM(Double_t pt, Double_t y, Double_t m, Double_t phi = -100);
+      /**
+       * generate TLorentzVector with given parameters
+       * @param pt - transversal momentum
+       * @param eta - pseudoapidity
+       * @param pid - PDG code
+       * @param phi - azimuthal angle if -100 generate random <-phi,phi>
+       * @return
+       */
+      TLorentzVector PtEta(Double_t pt, Double_t eta, Int_t pid, Double_t phi = -100);
+      /**
+       * generate TLorentzVector with given parameters
+       * @param pt
+       * @param eta
+       * @param m
+       * @param phi
+       * @return
+       */
+      TLorentzVector PtEtaM(Double_t pt, Double_t eta, Double_t m, Double_t phi = -100);
     }  // namespace LorentzGenerator
   }    // namespace Std
 }  // namespace Hal
